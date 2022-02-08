4efd1427f039c021307d5d63da2ad5686d989726
Jacob Lin <jacoblin@Jacobs-MBP.fios-router.home>
2022-02-08 01:57:00 -0500

    working nav

---
+++ catalog.js
@@ -0,0 +1,20 @@
+// contains catalog of products 
+var catalog = {
+    food: [
+        {name: "Potato", price: 5.99, description: "A beautiful potato", image: "./images/potato.jpeg"}
+    ], 
+    clothes: [
+        {name: "White T-Shirt", price: 25.00, description: "A plain white T-shirt", image: "./images/PWS.jpeg"}
+    ],
+    books: [
+        {name: "A Book", price: 10.00, description: "A pile of paper", image: "./images/book.jpeg"}
+    ]
+}
+
+    var best_sellers = [
+        {name: "Potato", price: 5.99, description: "A beautiful potato", image: "./images/potato.jpeg"},
+        {name: "White T-Shirt", price: 25.00, description: "A plain white T-shirt", image: "./images/PWS.jpeg"},
+        {name: "A Book", price: 10.00, description: "A pile of paper", image: "./images/book.jpeg"}
+    ]
+
+    
