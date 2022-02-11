56ea5277f0ae2a28a572c96f9c87e7a3d74552e2
Jacob Lin <jacoblin@Jacobs-MBP.fios-router.home>
2022-02-10 23:10:39 -0500

    JavaScript for Checkout added

--- catalog.js
+++ catalog.js
@@ -1,20 +1,15 @@
 // contains catalog of products 
 var catalog = {
-    food: [
-        {name: "Potato", price: "5.99", description: "A beautiful potato", image: "./images/potato.jpeg"}
-    ], 
-    clothes: [
-        {name: "White T-Shirt", price: "25.00", description: "A plain white T-shirt", image: "./images/PWS.jpeg"}
-    ],
-    books: [
-        {name: "A Book", price: "10.00", description: "A pile of paper", image: "./images/book.jpeg"}
-    ]
+    1: {name: "Potato", price: "5.99", description: "A beautiful potato", image: "./images/potato.jpeg"},
+    2: {name: "White T-Shirt", price: "25.00", description: "A plain white T-shirt", image: "./images/PWS.jpeg"},
+    3: {name: "A Book", price: "10.00", description: "A pile of paper", image: "./images/book.jpeg"}
+    
 }
 
-    var best_sellers = [
-        {name: "Potato", price: "5.99", description: "A beautiful potato", image: "./images/potato.jpeg"},
-        {name: "White T-Shirt", price: "25.00", description: "A plain white T-shirt", image: "./images/PWS.jpeg"},
-        {name: "A Book", price: "10.00", description: "A pile of paper", image: "./images/book.jpeg"}
-    ]
-
-    
+// track which product is in which category 
+var category = {
+    food: [1],
+    clothes: [2],
+    books: [3],
+    best_sellers: [1, 2, 3]
+}
