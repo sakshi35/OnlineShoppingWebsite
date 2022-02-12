9c4fefad30001aab84728cac4b2321423ceb0934
Jacob Lin <jacoblin@Jacobs-MBP.fios-router.home>
2022-02-12 12:55:20 -0500

    change from 3 products per row to 4

--- catalog.js
+++ catalog.js
@@ -2,14 +2,15 @@
 var catalog = {
     1: {name: "Potato", price: "5.99", description: "A beautiful potato", image: "./images/potato.jpeg"},
     2: {name: "White T-Shirt", price: "25.00", description: "A plain white T-shirt", image: "./images/PWS.jpeg"},
-    3: {name: "A Book", price: "10.00", description: "A pile of paper", image: "./images/book.jpeg"}
+    3: {name: "A Book", price: "10.00", description: "A pile of paper", image: "./images/book.jpeg"},
+    4: {name: "Cheetos", price: "1.99", description: "Dangerously cheesy", image: "./images/cheetos.jpeg"},
     
 }
 
 // track which product is in which category 
 var category = {
-    food: [1],
+    food: [1, 4],
     clothes: [2],
     books: [3],
-    best_sellers: [1, 2, 3]
+    best_sellers: [1, 2, 3, 4]
 }
