f935b1bb182b253f6bb225f0b2892e247e5de93f
sakshi35 <30382014+sakshi35@users.noreply.github.com>
2022-02-25 17:31:34 -0500

    Disabling the checkout button in cart on item-quantity change to 0

--- bag.js
+++ bag.js
@@ -1,15 +1,15 @@
 /**
- * code implements functionality for shopping bag 
- * this is for the UI portion of the shopping cart on the nav bar 
+ * code implements functionality for shopping bag
+ * this is for the UI portion of the shopping cart on the nav bar
  */
 
-// call the required functions to add something to cart 
+// call the required functions to add something to cart
 function addToBag(item){
     addToCart(item);
     updateBagCount();
 }
 
-// update the bag counter based on the number of items in the cart 
+// update the bag counter based on the number of items in the cart
 function updateBagCount(){
     console.log(cartCount);
     if (cartCount > 0){
