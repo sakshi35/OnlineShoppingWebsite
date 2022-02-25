d9cf0f27fe43033948d7c3adf441ec457dd8465a
sakshi35 <30382014+sakshi35@users.noreply.github.com>
2022-02-25 15:56:15 -0500

    Added Navbar to cart, Updated totalAmount, Editable itemQuantity

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,5 +1,5 @@
 /**
- * code implements functionality for shopping cart 
+ * code implements functionality for shopping cart
  */
 
 // add 1 of an item to cart
@@ -11,43 +11,62 @@ function addToCart(item_ID){
     else { // add 1 of that item to the cart
       cart[item_ID] = 1;
     }
-    cartCount++; // increment cart count 
-    // save to local storage 
+    cartCount++; // increment cart count
+    // save to local storage
     window.localStorage.setItem("cart", JSON.stringify(cart));
     window.localStorage.setItem("cartCount", JSON.stringify(cartCount));
     console.log("cart:" + window.localStorage.getItem("cart"));
 }
 
-// remove 1 of an item from cart 
+// remove 1 of an item from cart
 function subtractFromCart(item_ID){
-  cart[item_ID]--; 
+  cart[item_ID]--;
   cartCount--; // decrement cart count
 
   // delete item from cart if quantity falls to 0
   if (cart[item_ID] === 0){
     delete cart[item_ID];
   }
-  // save to local storage 
+  // save to local storage
   window.localStorage.setItem("cart", JSON.stringify(cart));
   window.localStorage.setItem("cartCount", JSON.stringify(cartCount));
   console.log("cart:" + window.localStorage.getItem("cartCount"));
 }
 
-// remove all of an item from cart 
+// remove all of an item from cart
 function removeFromCart(item_ID){
   cartCount -= cart[item_ID];
   delete cart[item_ID];
-  // save to local storage 
+  // save to local storage
   window.localStorage.setItem("cart", JSON.stringify(cart));
   window.localStorage.setItem("cartCount", JSON.stringify(cartCount));
   console.log("cart:" + window.localStorage.getItem("cart"));
   console.log("cart:" + window.localStorage.getItem("cartCount"));
 }
 
-// window.localStorage.clear();
+//update the quantity of an item in cart
+function updateItemQuantity(item_ID, quantity) {
+  cartCount = cartCount - cart[item_ID];
+  cartCount = cartCount + quantity;
+  if (quantity == 0) {
+    //If the updated quantity is 0, delete item from cart
+    removeFromCart(item_ID);
+    window.localStorage.setItem("cart", JSON.stringify(cart));
+    console.log("cart:" + window.localStorage.getItem("cart"));
+  }
+  else {
+    cart[item_ID] = quantity;
+    // save to local storage
+    window.localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  window.localStorage.setItem("cartCount", JSON.stringify(cartCount));
+}
+
+
+//window.localStorage.clear();
 
 // shopping cart made of ID and Quantity Pairs
-var cart; 
+var cart;
 var cartCount;
 
 if (window.localStorage.getItem("cart") === null){ // if cart is null, asign an empty cart
@@ -57,14 +76,14 @@ else{
   cart = JSON.parse(window.localStorage.getItem("cart")); // try to retrieve cart from local storage
 }
 
-if (window.localStorage.getItem("cartCount") == null){ 
+if (window.localStorage.getItem("cartCount") == null){
   cartCount = 0; // if cart count is empty set it to 0
-  for (id in cart){ // if cart is not empty but count is calculate count 
+  for (id in cart){ // if cart is not empty but count is calculate count
     cartCount += cart[id];
   }
 }
 else{
-  cartCount = JSON.parse(window.localStorage.getItem("cartCount")); // try to retrieve cart count from local storage 
+  cartCount = JSON.parse(window.localStorage.getItem("cartCount")); // try to retrieve cart count from local storage
 }
 
 console.log(cart)
